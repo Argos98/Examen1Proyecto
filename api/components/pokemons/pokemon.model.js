01332d6fe46bd4ff7a26b379dcaade30657b2158
David Serrano Gutierrez <davidsegu9@gmail.com>
2018-07-22 03:30:13 -0600

    commit add model
    todo listo para registrar pokemon solo falta la parte del back end

---
+++ api/components/pokemons/pokemon.model.js
@@ -0,0 +1,14 @@
+'use strict';
+let mongoose = require('mongoose');
+
+let pokemonSchema = new mongoose.Schema({
+    numero_pokedex: { type: String, required: true },
+    nombre_pokemon: { type: String, required: true },
+    tipo_1: { type: String, require: true },
+    tipo_2: { type: String, required: false },
+    foto: { type: String, require: true }
+
+
+});
+
+module.exports = mongoose.model('Pokemon', adminSchema);
--- public/js/pokemons/controlador.registrarPokemon.js
+++ public/js/pokemons/controlador.registrarPokemon.js
@@ -1,8 +1,12 @@
 'use strict';
-
+listarSelectTipo1();
+listarSelectTipo2();
 let botonRegistrar = document.querySelector('#btnRegistrar');
 
 botonRegistrar.addEventListener('click', obtnerDatos);
+
+
+
 
 
 let inputNumeroPokedex = document.querySelector('#txtNumeroPokedex');
@@ -17,22 +21,50 @@ let buttonSubirFoto = document.querySelector('#subirFoto');
 
 function obtnerDatos() {
 
-    let infoPokemon=[];
+    let infoPokemon = [];
 
-let nNumeroPokedex = inputNumeroPokedex.value;
+    let nNumeroPokedex = inputNumeroPokedex.value;
 
-let sNombrePokemon = inputNombrePokemon.value;
+    let sNombrePokemon = inputNombrePokemon.value;
 
-let sTipo1 = selectTipo1.value;
+    let sTipo1 = selectTipo1.value;
 
-let sTipo2 = selectTipo2.value
+    let sTipo2 = selectTipo2.value
 
-let urlFotoPokemon = imgUrl;
- 
-console.log(urlFotoPokemon)
+    let urlFotoPokemon = imgUrl;
 
-infoPokemon.push(nNumeroPokedex, sNombrePokemon, sTipo1, sTipo2, urlFotoPokemon);
+    console.log(urlFotoPokemon)
+
+    infoPokemon.push(nNumeroPokedex, sNombrePokemon, sTipo1, sTipo2, urlFotoPokemon);
 
 
 
+}
+
+
+
+
+function listarSelectTipo1() {
+    let tipo =['Acero', 'Agua', 'Bicho', 'Dragón', 'Eléctrico', 'Fantasma', 'Fuego', 'Hada', 'Hielo', 'Lucha', 'Normal', 'Planta', 'Psíquico', 'Roca', 'Siniestro', 'Tierra', 'Veneno', 'Volador'];
+    let select = document.querySelector('#sltTipo1');
+    select.options[0] = new Option("Seleccione un tipo");
+
+    for (let i = 0; i < tipo.length; i++) {
+        
+        select.options[i+1] = new Option(tipo[i]);
+
+    }
+}
+
+
+function listarSelectTipo2() {
+    let tipo =['Acero', 'Agua', 'Bicho', 'Dragón', 'Eléctrico', 'Fantasma', 'Fuego', 'Hada', 'Hielo', 'Lucha', 'Normal', 'Planta', 'Psíquico', 'Roca', 'Siniestro', 'Tierra', 'Veneno', 'Volador'];
+    let select = document.querySelector('#sltTipo2');
+    select.options[0] = new Option("Seleccione un tipo");
+
+    for (let i = 0; i < tipo.length; i++) {
+        
+        select.options[i+1] = new Option(tipo[i]);
+
+    }
 }
